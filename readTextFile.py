1971a31f0b3cd46de97ecc95c2cc6c77a3e7fd00
Jaydan <871138649@qq.com>
2017-10-30 09:19:43 +0800

    add make...read...

---
+++ readTextFile.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'readTextFile.py -- read and display text file'
+
+#get filename
+fname = raw_input('Enter:')
+print
+
+#attempt to open file for reading
+try:
+	fobj = open(fname,'r')
+except IOError,e:
+	print "***file open error:",e
+else:
+	#display contents to the screen
+	for eachLine in fobj:
+		print eachLine,
+		fobj.close()
